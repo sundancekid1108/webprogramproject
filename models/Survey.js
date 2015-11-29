af5c3843a659377ceb1993572675d38b7e4c00b3
sundancekid1108 <sundancekid1108@gmail.com>
2015-11-29 15:35:58 +0900

    "survey 데이터 스키마 제작"

---
+++ models/Survey.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var schema = new Schema({
+	
+});
+
+var Survey = mongoose.model("Survey", schema);
+
+module.exports = Survey;
